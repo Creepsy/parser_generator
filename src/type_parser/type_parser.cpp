dc2a9314db5f09d2ecb337ab921e66dfd300147b
Creepsy <Robin.gd@outlook.de>
2022-03-03 20:26:06 +0100

    refactoring

--- src/type_parser/type_parser.cpp
+++ src/type_parser/type_parser.cpp
@@ -10,34 +10,34 @@ TypeParser::TypeParser(type_lexer::type_lexer& lexer) : ParserBase(lexer) {
 }
 
 TypeDefinition TypeParser::parse_next_type() {
-    std::string type_identifier = this->consume(type_lexer::token::token_type::IDENTIFIER).identifier;
+    std::string type_identifier = this->consume(type_lexer::token::IDENTIFIER).identifier;
 
-    this->consume(type_lexer::token::token_type::PAR_OPEN);
+    this->consume(type_lexer::token::PAR_OPEN);
 
     //parse base types
     std::set<std::string> base_types;
     
-    while(!this->accept(type_lexer::token::token_type::PAR_CLOSE)) {
+    while(!this->accept(type_lexer::token::PAR_CLOSE)) {
         if(!base_types.empty()) 
-            this->consume(type_lexer::token::token_type::SEPERATOR);
+            this->consume(type_lexer::token::SEPERATOR);
 
-        base_types.insert(this->consume(type_lexer::token::token_type::IDENTIFIER).identifier);
+        base_types.insert(this->consume(type_lexer::token::IDENTIFIER).identifier);
     }
 
-    this->consume(type_lexer::token::token_type::PAR_CLOSE);
-    this->consume(type_lexer::token::token_type::ASSIGN);
+    this->consume(type_lexer::token::PAR_CLOSE);
+    this->consume(type_lexer::token::ASSIGN);
 
     //parse parameters
     std::vector<Parameter> parameters;
 
-    while(!this->accept(type_lexer::token::token_type::EOL)) {
+    while(!this->accept(type_lexer::token::EOL)) {
         if(!parameters.empty()) 
-            this->consume(type_lexer::token::token_type::SEPERATOR);
+            this->consume(type_lexer::token::SEPERATOR);
 
         parameters.push_back(this->parser_parameter());
     }
 
-    this->consume(type_lexer::token::token_type::EOL);
+    this->consume(type_lexer::token::EOL);
 
     return TypeDefinition{type_identifier, base_types, parameters};    
 }
@@ -49,12 +49,12 @@ TypeParser::~TypeParser() {
 
 //private
 Parameter TypeParser::parser_parameter() {
-    std::string name = this->consume(type_lexer::token::token_type::IDENTIFIER).identifier;
-    this->consume(type_lexer::token::token_type::ASSIGN);
-    std::string type = this->consume(type_lexer::token::token_type::IDENTIFIER).identifier;
+    std::string name = this->consume(type_lexer::token::IDENTIFIER).identifier;
+    this->consume(type_lexer::token::ASSIGN);
+    std::string type = this->consume(type_lexer::token::IDENTIFIER).identifier;
     bool is_vector = false;
 
-    if(this->accept(type_lexer::token::token_type::VECTOR)) {
+    if(this->accept(type_lexer::token::VECTOR)) {
         is_vector = true;
         this->consume();
     }
